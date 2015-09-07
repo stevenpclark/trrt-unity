fd352c59d7ba16ab7202d9d78523a36a177d2c89
RumBull <steven.p.clark@gmail.com>
2015-09-07 14:19:50 -0400

    start & goal added, with corresponding materials

--- Assets/TerrainHeightFollower.cs
+++ Assets/TerrainHeightFollower.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using System.Collections;
 
 public class TerrainHeightFollower : MonoBehaviour {
+	public float heightOffset = 0.1f;
 
 	// Use this for initialization
 	void Start () {
@@ -16,7 +17,7 @@ public class TerrainHeightFollower : MonoBehaviour {
 	void LateUpdate () {
 		Vector3 pos = transform.position;
 		
-		pos.y = Terrain.activeTerrain.SampleHeight(pos)+20;
+		pos.y = Terrain.activeTerrain.SampleHeight(pos)+heightOffset;
 		
 		transform.position = pos;
 	}
